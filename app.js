2daf35517f930b8e3b49dbda0f778ca53aae15f7
Shivani Shingole <shivanishingole@gmail.com>
2025-04-08 16:39:31 +0530

    Added week 1 task

---
+++ app.js
@@ -0,0 +1,5 @@
+// Simple interaction using JavaScript
+document.getElementById("greetBtn").addEventListener("click", function () {
+    alert("Thanks for clicking! 🎉 You're doing great!");
+  });
+  
